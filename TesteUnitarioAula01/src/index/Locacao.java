c96a4a6dc78b713bc89cdd4a112b156e9b6e5252
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2022-09-07 20:58:08 -0300

    feat: adição do desconto no valor do filme por genero e do método de top genero mais alugados

--- TesteUnitarioAula01/src/index/Locacao.java
+++ TesteUnitarioAula01/src/index/Locacao.java
@@ -6,24 +6,48 @@ import java.util.Calendar;
 public class Locacao implements Pagamento {
 	protected Cliente cliente;
 	protected Filme filme;
-	protected double valorAluguel;
+	protected double valorPagoNoAluguel;
 	protected Calendar data;
 	protected  ArrayList<Locacao> alugueis;
+	protected GeneroENUM topsGeneros;
 	
 	@Override
 	public void pagar(double valorDoPagamento) {
-		this.valorAluguel = valorDoPagamento;
+		this.valorPagoNoAluguel = valorDoPagamento;
 	}
 	
-	public void alugar(Cliente c, Filme f, double valorAluguel) {
-		if((valorAluguel == f.getValorCompra()) || (c.statusAtivo==true)) {
+	public void alugar(Cliente c, Filme f, double valorDoPagamento) {
+		if((valorDoPagamento == f.getValorLocacao()) || (c.statusAtivo==true)) {
 			this.cliente = c;
 			this.filme = f;
-			pagar(valorAluguel);
+			pagar(valorDoPagamento);
 			alugueis= new ArrayList<Locacao>();
 			this.data = Calendar.getInstance();
 		}else {
 			System.out.println("Não foi possivel alugar o filme, valor insuficiente ou cliente inativo");
 		}
+	}
+	
+	public void setDescontoPorGenero(GeneroENUM generoENUM, double valorDoDesconto ) {
+		for (Locacao locacao : alugueis) {
+			if(locacao.filme.generoENUM==generoENUM) {
+				locacao.filme.setValorLocacao(locacao.filme.getValorLocacao() - valorDoDesconto);
+			}
+		}
+	}
+	
+	private void setTopMaisAlugados() {
+		for (GeneroENUM generos: GeneroENUM.values()) {
+			for (Locacao locacao : alugueis) {
+				if(locacao.filme.generoENUM==generos) {
+					
+				}
+			}
+		}
+	}
+	
+	public GeneroENUM getTopMaisAlugados() {
+		setTopMaisAlugados();
+		return this.topsGeneros;
 	}
 }
